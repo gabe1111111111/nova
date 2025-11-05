a57bf8ed7b19ec6b3e664beaf0b2149ea4019d46
Gabe-lacey <gvlacey@uwm.edu>
2025-11-05 13:58:53 -0600

    optimized absolute value

--- ProjectName/src/main/java/gabriel/nova/Math.java
+++ ProjectName/src/main/java/gabriel/nova/Math.java
@@ -1,5 +1,5 @@
 package gabriel.nova;
 
 public class Math {
-    public static int absoluteValue(int in){return in < 0 ? -in : in;}
+    public static int absoluteValue(int in){return in & 0x7fffffff;}
 }
