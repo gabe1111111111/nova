9194a7c8717f0c731d5bb1a514909ba3aa377436
Gabe-lacey <gvlacey@uwm.edu>
2025-11-05 12:26:02 -0600

    created skeliton hash map

---
+++ ProjectName/src/main/java/gabriel/nova/HashMap.java
@@ -0,0 +1,11 @@
+package gabriel.nova;
+
+public class HashMap<T> {
+    public T[] data;
+    public int size;
+
+    public HashMap() {
+    }
+    public boolean  contains(T target){return false;}
+    public void add (T addend){}
+}
