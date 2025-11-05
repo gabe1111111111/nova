e272dac7a7a0f8cb3c439319d0aa6063666a2a6c
Gabe-lacey <gvlacey@uwm.edu>
2025-11-05 11:54:23 -0600

    created and tested load program

--- ProjectName/src/main/java/gabriel/nova/Compiler.java
+++ ProjectName/src/main/java/gabriel/nova/Compiler.java
@@ -1,18 +1,39 @@
 package gabriel.nova;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * this class handles the whole compilation process 
  * @author Gabriel Lacey
  */
-import java.util.ArrayList;
 public class Compiler{
-public ArrayList<String> test;
-public int loadProgram(){return 0;}
-public int preProcessor(){return 0;}
-public int lexer(){return 0;}
-public int parser(){return 0;}
-public int codeGeneration(){return 0;}
-public int flowChart(){return 0;}
-public int assemblyGeneration(){return 0;}
-public int assembler(){return 0;}
+public ArrayList<String> sourceCode;
+
+    public Compiler() {
+    sourceCode = new ArrayList<>();
+    }
+    public Error loadProgram(String directory){
+        try (BufferedReader reader = new BufferedReader(new FileReader(directory))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sourceCode.add(line);
+                }
+            } catch (FileNotFoundException e) {
+                return Error.__FILE_NOT_FOUND_EXCEPTION__;
+            } catch(IOException e){
+                return Error.__UNEXPECTED_ERROR_WHILE_LOADING_FILE_;
+            }
+        return Error.__NO_ERROR__;
+    }
+    public Error preProcessor(){return Error.__NO_ERROR__;}
+    public Error lexer(){return Error.__NO_ERROR__;}
+    public Error parser(){return Error.__NO_ERROR__;}
+    public Error codeGeneration(){return Error.__NO_ERROR__;}
+    public Error flowChart(){return Error.__NO_ERROR__;}
+    public Error assemblyGeneration(){return Error.__NO_ERROR__;}
+    public Error assembler(){return Error.__NO_ERROR__;}
 
 }
