255b67e1ec4ca090fee009fd4ca04ddafe189ebb
Gabe-lacey <gvlacey@uwm.edu>
2025-11-04 18:11:01 -0600

    created skeleton of compiler

--- ProjectName/src/main/java/gabriel/nova/Compiler.java
+++ ProjectName/src/main/java/gabriel/nova/Compiler.java
@@ -6,5 +6,13 @@ package gabriel.nova;
 import java.util.ArrayList;
 public class Compiler{
 public ArrayList<String> test;
+public int loadProgram(){return 0;}
+public int preProcessor(){return 0;}
+public int lexer(){return 0;}
+public int parser(){return 0;}
+public int codeGeneration(){return 0;}
+public int flowChart(){return 0;}
+public int assemblyGeneration(){return 0;}
+public int assembler(){return 0;}
 
 }
